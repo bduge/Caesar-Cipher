52f8dab29c15dd496d63b1758ca03ef63207b902
Bill DuGe <billduge11@gmail.com>
2018-09-15 21:45:21 -0400

    getData() update
    getData() function now ensures that the code shift input is a number using the javascript function isNan(value).

--- Caesar.js
+++ Caesar.js
@@ -3,9 +3,15 @@ var codeString
 var codeShift
 
 function getData(){
+
+    if(isNaN(document.getElementById("codeShift").value)){
+        alert("The code shift must be an integer")
+    }
     
-    codeString = document.getElementById("codeString").value;
-    codeShift = document.getElementById("codeShift").value;
+    else{
+        codeString = document.getElementById("codeString").value;
+        codeShift = document.getElementById("codeShift").value;
+    }
 
     console.log(codeString)
     console.log(codeShift)
